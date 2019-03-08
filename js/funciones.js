ff0de0a41443fbac0f7d8d9741cf80d76feecf54
Kevin Grassi <48366759+kevinb45@users.noreply.github.com>
2019-03-08 18:00:25 -0300

    JS
    Carpeta subida

---
+++ js/funciones.js
@@ -0,0 +1,8 @@
+function eliminar(url){
+    
+	if(window.confirm("Realmente desea eliminar este registro?")){
+        window.location=url;
+    }
+	
+}
+
---
+++ js/login.js
@@ -0,0 +1,19 @@
+//Esta función se encarga del checkbox de contraseña para mostrar lo que uno escribe.
+function showPassword() {
+    
+    var key_attr = $('#key').attr('type');
+    
+    if(key_attr != 'text') {
+        
+        $('.checkbox').addClass('show');
+        $('#key').attr('type', 'text');
+        
+    } else {
+        
+        $('.checkbox').removeClass('show');
+        $('#key').attr('type', 'password');
+        
+    }
+    
+}
+
